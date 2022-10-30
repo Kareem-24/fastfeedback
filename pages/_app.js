2e3e023d631234f96a5d59c4ab61d9086f4278c1
kareem <abdelkareem58@gmail.com>
2022-10-31 01:01:48 +0200

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,34 @@
-import { ProvideAuth } from "../lib/auth";
+import { AuthProvider } from "@/lib/auth";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "@/styles/theme";
 
-import "../styles/globals.css";
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <ProvideAuth>
+      <AuthProvider>
         <Component {...pageProps} />
-      </ProvideAuth>
+      </AuthProvider>
     </ChakraProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,35 @@
-import { useAuth } from "../lib/auth";
+import { useAuth } from "@/lib/auth";
+import { Button, Heading, Text, Code, Icon, Flex } from "@chakra-ui/react";
+import theme from "@/styles/theme";
+import Head from "next/head";
+
 export default function Home() {
   const auth = useAuth();
   return (
-    <div>
-      <main>
-        <h1>Fast Feedback</h1>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    >
+      <Head>
+        <title>Fast Feedback</title>
+      </Head>
 
-        <button onClick={(e) => auth.signinWithGitHub()}>Sign In</button>
-        {auth?.user && (
-          <button onClick={(e) => auth.signout()}>Sign Out</button>
-        )}
+      <Icon color="black" boxSize={16} viewBox="0 0 46 32">
+        <path
+          fill="currentColor"
+          d="M19.557.113C11.34.32 9.117 8.757 9.03 12.95c1.643-2.67 4.62-3.08 6.931-3.08 2.825.085 10.27.205 17.458 0C40.61
+            9.663 44.802 3.28 46 .112c-5.391-.085-18.228-.205-26.443 0zM14.422 14.234C3.332 14.234-.468 24.76.045 31.948c3.594-6.418
+              7.617-7.53 9.243-7.445h6.675c5.956 0 11.039-6.846 12.836-10.27H14.422z"
+        />
+      </Icon>
 
-        <div>{auth?.user?.email}</div>
-      </main>
-    </div>
+      <Button mt={4} size="sm" onClick={(e) => auth.signinWithGitHub()}>
+        Sign In
+      </Button>
+      {auth?.user && <Button onClick={(e) => auth.signout()}>Sign Out</Button>}
+    </Flex>
   );
 }
